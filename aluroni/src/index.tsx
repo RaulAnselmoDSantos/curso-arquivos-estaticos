6f413b78f0d6b83ec338433da3ded87e7519cf41
RaulAnselmoDSantos <raul.trabalho5511@gmail.com>
2024-01-18 19:41:49 -0300

    fix: remving imports that is not in use

--- aluroni/src/index.tsx
+++ aluroni/src/index.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Cardapio from './pages/Cardapio';
-import Botao from './components/Botao';
 import 'normalize.css'; 
-import Inicio from 'pages/Inicio/Index';
-import { Router } from 'react-router-dom';
 import { AppRouter } from 'routes';
 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
-const componenteAtual = window.location.pathname === '/' ?  <Inicio/> : <Cardapio/>
 
 root.render(
 	<React.StrictMode>
