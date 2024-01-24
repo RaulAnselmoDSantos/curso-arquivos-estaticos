1182d1560f01a095a736c714c04ad23edb2981bd
RaulAnselmoDSantos <raul.trabalho5511@gmail.com>
2024-01-24 11:32:26 -0300

    feat: creation and ajustments of the page Sobre

--- aluroni/src/pages/Sobre/index.tsx
+++ aluroni/src/pages/Sobre/index.tsx
@@ -1,15 +1,46 @@
-import style from './Sobre.module.scss';
+import style from "./Sobre.module.scss";
+import stylesTema from "styles/Tema.module.scss";
+import casa from 'pages/Sobre/sobre/casa.png';
+import massa1 from 'pages/Sobre/sobre/massa1.png';
+import massa2 from 'pages/Sobre/sobre/massa2.png';
+
+const imagens = [massa1 , massa2];
 
 export default function Sobre() {
-    return(
-        <div className={style.sobreNos}> 
-
-            <p className={style.sobreNos__texto}>
-
-            </p>
-            <div className={style.imagens} >
-                <img className={style.imagens__imagem} src={''} />
+    
+  return (
+    <section>
+        <h3 className={stylesTema.titulo}>Sobre</h3><br/>
+    <div className={style.sobreNos}>
+      
+        <img src={casa} alt={"Casa do Aluroni"} />
+      
+      <div className={style.sobreNos__texto}>
+        <p>
+          Nós do Aluroni oferecemos a vocês, nossos queridos clientes, a Massa
+          Italiana Caseira mais saborosa e sofisticada de São Paulo! Prezamos
+          pelos ingredientes tradicionais da culinária Italiana, frescos e de
+          excelente qualidade para que sua experiência seja ainda mais intensa!
+        </p>
+        <p>
+          Também possuímos uma cardápio de carnes com muitas opções de acordo
+          com o seu gosto!
+        </p>
+        <p>
+          Para acompanhar as massas italianas, Aluroni possui uma reserva de
+          vinhos especiais, que harmonizam perfeitamente com o seu parto, seja
+          carne ou massa!
+        </p>
+      </div>
+      
+    </div>
+    <div className={style.imagens}>
+        {imagens.map((imagens, index) => (
+            <div key={index} className={style.imagens__imagem}>
+                <img  src={imagens} alt="imagem de massa" />
             </div>
-        </div>
-    )
+        ))}
+      </div>
+    </section>
+  );
 }
