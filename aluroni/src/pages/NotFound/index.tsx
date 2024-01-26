5acdf6fe0e90a4577019037cac9fe5106077bd97
RaulAnselmoDSantos <raul.trabalho5511@gmail.com>
2024-01-26 13:21:49 -0300

    feat: refactory of the page NotFound

--- aluroni/src/pages/NotFound/index.tsx
+++ aluroni/src/pages/NotFound/index.tsx
@@ -1,7 +1,17 @@
 import { ReactComponent as NFImage } from 'assets/not_found.svg';
+import style from './NotFound.module.scss';
+import Botao from 'components/Botao';
+
+
 
 export default function NotFound(){
     return(
-        <NFImage/>
+        <div className={style.container}>
+            <NFImage/>
+            <div className={style.voltar}>
+                    
+                    <Botao texto='Voltar'/>               
+            </div>
+        </div>
     )
 }
